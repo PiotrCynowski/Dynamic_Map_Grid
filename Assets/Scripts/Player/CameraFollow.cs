a45d5bfa357abe7e81f391a0f8d4a2f91db4810c
Piotr Cynowski <piotr.peter.cynowski@gmail.com>
2025-05-05 21:19:54 +0200

    update

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 
-namespace Player.Camera {
-    public class CameraFollow : MonoBehaviour {
-        [SerializeField] Transform target;
-        [SerializeField] float smoothSpeed;
-        [SerializeField] Vector3 offset;
-        [SerializeField] bool keepTarget = true;
+namespace Player
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float smoothSpeed;
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private bool keepTarget = true;
 
-        Vector3 desiredPosition, smoothedPosition;
+        private Vector3 desiredPosition, smoothedPosition;
 
-        void LateUpdate() {
-            if (keepTarget == true) {
+        private void LateUpdate()
+        {
+            if (keepTarget == true)
+            {
                 desiredPosition = target.position + offset;
                 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
                 transform.position = smoothedPosition;
